b918bfea8e57b6f997feab9b4bc1df3dd6aecdaa
mirror6 <1766650501@qq.com>
2019-09-09 23:32:59 +0800

    ArrayList接口源码

---
+++ src/cn/mirror6/source/collection/list/ListTest.java
@@ -0,0 +1,25 @@
+package cn.mirror6.source.collection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * @author mirror6
+ * @date 2019/9/9 18:44
+ */
+public class ListTest {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+//        list.trm
+
+        List list2 = new LinkedList();
+
+        List list3 = new Vector();
+    }
+
+
+}
